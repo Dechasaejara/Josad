81889a8a0faec7345a308a29e368fec05fbfba52
D <d@Ds-MacBook-Pro.local>
2024-01-08 20:06:54 +0300

    fetch  job and display

--- src/app/api/(services)/jobs/[jobid]/route.ts
+++ src/app/api/(services)/jobs/[jobid]/route.ts
@@ -1,7 +1,17 @@
 import prisma from "@/lib/prisma";
 import { Job } from "../route";
 
+// Get by id
 
+export async function GET(request: Request, { params }: { params: { jobid: string } }) {
+    console.log('id', params);
+    const job = await prisma.job.findUnique({
+        where: {
+            id: +params.jobid,
+        }
+    });
+    return new Response(JSON.stringify(job));
+}
 // update
 export async function PUT(request: Request, { params }: { params: { jobid: string } }) {
     const body: Job = await request.json();
@@ -15,7 +25,7 @@ export async function PUT(request: Request, { params }: { params: { jobid: strin
 
     return new Response(JSON.stringify(updatejob));
 }
-
+// Delete
 export async function DELETE(request: Request, { params }: { params: { jobid: string } }) {
     console.log("id", params);
     let deletedJob = await prisma.job.delete({
