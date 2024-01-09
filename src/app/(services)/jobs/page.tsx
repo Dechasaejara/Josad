7a314cf6474db7d8bb1684efddb9970617c83432
D <d@Ds-MacBook-Pro.local>
2024-01-09 22:31:22 +0300

    from josad

--- src/app/(services)/jobs/page.tsx
+++ src/app/(services)/jobs/page.tsx
@@ -1,9 +1,37 @@
+import { MDXProvider } from "@mdx-js/react";
+import { JSX, ClassAttributes, HTMLAttributes } from "react";
+import { JSX, ClassAttributes, HTMLAttributes } from "react";
 
-const baseUrl = 'https://josad-service.onrender.com';
-const baseUrll = 'http://localhost:3000/';
-const apiKey = 'd0205fb4-oihh-0282-npth-e36a6498039c';
+interface Post {
+  createdAt: string;
+  id: number;
+  views: number;
+  message: string;
+  forwards: number;
+  reactions: {
+    flags: number;
+    chosenOrder: null;
+    reaction: {
+      emoticon: string;
+      className: string;
+    };
+    count: number;
+    className: string;
+  }[];
+  originalSource: string;
+}
+
+interface ChannelPosts {
+  channel: string;
+  posts: Post[];
+}
+
+
+
+
+const baseUrl = process.env.JOSAD_BASE_URL as string;
+const apiKey = process.env.JOSAD_API_KEY as string;
 export default async function Jobs() {
-  console.log("apiKey", apiKey);
   const dynamicData = await fetch(`${baseUrl}/api/jobs`, {
     method: "GET",
     cache: 'no-store',
@@ -12,26 +40,33 @@ export default async function Jobs() {
       'x-api-key': apiKey,
     },
   });
-  const Key = dynamicData.headers.get('apiKey');
-  console.log("key", Key);
-  console.log('dynamicData', dynamicData);
   const data = await dynamicData.json();
-  // console.log('message', message);
-  console.log('data', data);
+  const messages: string[] = data.flatMap((channel: { posts: any[]; }) => channel.posts.map((post: { message: any; }) => post.message));
+  console.log("messages", messages);
+  const components = {
+    // Define any custom components you want to use in the MDX file here
+    // For example:
+    h1: (props: JSX.IntrinsicAttributes & ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => <h1 style={{ color: 'red' }} {...props} />,
+    p: (props: JSX.IntrinsicAttributes & ClassAttributes<HTMLParagraphElement> & HTMLAttributes<HTMLParagraphElement>) => <p style={{ fontSize: '16px' }} {...props} />,
+  };
   return (
     <main>
       <h1>Jobs</h1>
       <ul className="flex flex-col gap-4 h-screen ">
-        {/* {data.map((job: Job) => (
-          <div
-            key={job.title}
-            className="p-8 m-auto container bg-secondary w-2/3"
-          >
-            <h2>{job.title}</h2>
-            <p>{job.description}</p>
-  
-          </div>
-        ))} */}
+        {
+          messages && messages.map((job: any, index) => (
+            <div
+              key={index}
+              className="p-8 m-auto container bg-secondary w-2/3"
+            >
+              <div>{job}</div>
+              {/* <div>{job.replace(/\n/g, '<br />')}</div> */}
+              {/* <MDXProvider components={components}>
+                <div>{job}</div>
+              </MDXProvider> */}
+            </div>
+          ))
+        }
       </ul>
     </main>
   );
